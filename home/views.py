f30e47a7b6a6ee85ac61b8708729971964fe1d2a
zahra imen <zahraimen91@gmail.com>
2023-08-18 15:29:50 +0330

    create serializers and set view

--- home/views.py
+++ home/views.py
@@ -2,12 +2,16 @@ from django.shortcuts import render
 # from django.views import View
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from .serializers import PersonSerializer
+from .models import Person
 
 # Create your views here.
 class Home(APIView):
     def get(self,request):
-        name=request.query_params['name']
-        return Response({'name':name})
+        # name=request.query_params['name']
+        persons = Person.objects.all()
+        ser_data=PersonSerializer(instance=persons,many=True)
+        return Response(data=ser_data.data)
     # def post(self,request):
     #     name=request.data['name']
     #     return Response({'name':name})
