ab7747be3df26d3ddd515898523cbd64c8087df1
zahra imen <zahraimen91@gmail.com>
2023-08-19 21:20:09 +0330

    add write only to password seria;izer field

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -3,4 +3,4 @@ from rest_framework import serializers
 class UserRegisterSerializer(serializers.Serializer):
     username=serializers.CharField(required=True)
     email=serializers.EmailField(required=True)
-    password=serializers.CharField(required=True)
+    password=serializers.CharField(required=True,write_only=True)
