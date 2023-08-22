14eac9de40bdd558e516c97152e177132a692b0b
zahra imen <zahraimen91@gmail.com>
2023-08-22 10:45:22 +0330

    add validator to serializers

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,14 +1,24 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
 def clean_email(value):
     if 'admin' in value:
         raise serializers.ValidationError('not admin in email')
 
-class UserRegisterSerializer(serializers.Serializer):
-    username=serializers.CharField(required=True)
-    email=serializers.EmailField(required=True,validators=[clean_email])
-    password=serializers.CharField(required=True,write_only=True)
+class UserRegisterSerializer(serializers.ModelSerializer):
     password2=serializers.CharField(required=True,write_only=True)
+    class Meta:
+        model=User
+        fields=('username','email','password','password2')
+        extra_kwargs:{
+            'password':{'write_only':True},
+            'email':{'validators':(clean_email)}
+        }
+        
+    # username=serializers.CharField(required=True)
+    # email=serializers.EmailField(required=True,validators=[clean_email])
+    # password=serializers.CharField(required=True,write_only=True)
+    # password2=serializers.CharField(required=True,write_only=True)
     
     def validate_username(self,value):
         if value == 'admin':
