123801df5b8ec004f42e8adce9bec859fad1c2ee
zahra imen <zahraimen91@gmail.com>
2023-08-22 14:59:09 +0330

    ovverride create function and add status code

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -10,11 +10,15 @@ class UserRegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model=User
         fields=('username','email','password','password2')
-        extra_kwargs:{
-            'password':{'write_only':True},
-            'email':{'validators':(clean_email)}
+        extra_kwargs = {
+            'password' : {'write_only':True},
+            'email' : {'validators':(clean_email,)}
         }
         
+        
+    def create(self, validated_data):
+       del validated_data['password2']  
+       return User.objects.create_user(**validated_data)
     # username=serializers.CharField(required=True)
     # email=serializers.EmailField(required=True,validators=[clean_email])
     # password=serializers.CharField(required=True,write_only=True)
--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.contrib.auth.models import User
 from .serializers import UserRegisterSerializer
+from rest_framework import status
 
 
 # Create your views here.
@@ -10,12 +11,8 @@ class UserRegisterView(APIView):
     def post(self,request):
         ser_data=UserRegisterSerializer(data=request.POST)
         if ser_data.is_valid():
-            User.objects.create_user(
-              username=ser_data.validated_data['username'],
-              email=ser_data.validated_data['email'],
-              password=ser_data.validated_data['password'],
-            )
+            ser_data.create(ser_data._validated_data)
             print(ser_data)
             print(ser_data.data)
-            return Response(ser_data.data)
-        return Response(ser_data.errors)
+            return Response(ser_data.data,status=status.HTTP_201_CREATED)
+        return Response(ser_data.errors,status=status.HTTP_400_BAD_REQUEST)
